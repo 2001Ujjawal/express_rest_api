e6b9faf49ae722193f85ff4e001ce1113aa1a173
Ujjawal Sharma <ujjawalkpa09@gmail.com>
2026-03-14 15:26:38 +0530

    remove extra function

--- services/user.services.js
+++ services/user.services.js
@@ -3,33 +3,6 @@ const model = require("../models");
 const commonUtil = require("../utils/common.utils");
 const bcrypt = require("bcrypt");
 
-async function createUser(requestData) {
-  try {
-    const { name, email, mobile, password } = requestData;
-    const existingUser = await model.UserModel.findOne({
-      where: { email },
-    });
-
-    if (existingUser) {
-      const error = new Error("Email already exists");
-      error.statusCode = 409;
-      throw error;
-    }
-    const hashPassword = await bcrypt.hash(password, 10);
-    return await model.UserModel.create({
-      full_name: name,
-      uid: await commonUtil.generateUid(),
-      email,
-      password: hashPassword,
-      mobile,
-    });
-  } catch (error) {
-    throw error;
-  }
-}
-
-//////////////////////////////////////////////////////////
-
 async function userRegister(data) {
   try {
     const { name, email, password, mobile } = data;
